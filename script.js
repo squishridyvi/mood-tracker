61a31f1e839e81d97d182922f96ce432bdfd86a5
Sridevi Hariharan <100106579+squishridyvi@users.noreply.github.com>
2026-09-04 11:37:57 -0500

    Add files via upload
    Centered scan-button and spinner as inline elements in .input-area, added result.html file as redirected page when scan is clicked

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".scan-button img").addEventListener("click", function() {
-        document.querySelector(".spinner").style.display = "block";
+        document.querySelector(".spinner").style.display = "inline-block";
         setTimeout(function() {
             document.querySelector(".spinner").style.display = "none";
+            window.location.href = "result.html";
         }, 3000);
+
+
     });
 });
 
