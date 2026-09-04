bf37cef670a709d82ebd8e00132a25690804a46c
Sridevi Hariharan <100106579+squishridyvi@users.noreply.github.com>
2026-09-04 02:17:39 -0500

    connected to script.js file

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".scan-button img").addEventListener("click", function() {
+        alert("Scan button clicked!");
+    });
+});
+
