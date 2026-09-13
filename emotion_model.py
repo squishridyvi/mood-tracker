0053b6b3dbea392396fc75a6e8ed5ed3625efa05
Sridevi Hariharan <100106579+squishridyvi@users.noreply.github.com>
2026-09-12 23:49:14 -0500

    Add files via upload
    made emotion model into FastAPI server with POST /analyze-emotion route

--- emotion_model.py
+++ emotion_model.py
@@ -1,5 +1,16 @@
 from transformers import pipeline
+from fastapi import FastAPI 
+from pydantic import BaseModel
 
-# Load the emotion analysis pipeline
+# Loading the emotion analysis pipeline
 emotion_analyzer = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None)
-print(emotion_analyzer("I am feeling great today!"))
+
+# Creating a FastAPI app
+class Entry(BaseModel):
+    text: str
+    
+app = FastAPI()
+
+@app.post("/analyze-emotion")
+def analyze_emotion(entry: Entry):
+    return emotion_analyzer(entry.text)[0]
