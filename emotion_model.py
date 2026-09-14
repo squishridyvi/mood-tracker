08e8877a98ddbe599d00bc83321ad37aa84ee84a
Sridevi Hariharan <100106579+squishridyvi@users.noreply.github.com>
2026-09-13 23:27:58 -0500

    Add files via upload
    Sent entry from scan button to FastAPI server, enabled CORS

--- emotion_model.py
+++ emotion_model.py
@@ -1,15 +1,25 @@
 from transformers import pipeline
 from fastapi import FastAPI 
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 # Loading the emotion analysis pipeline
 emotion_analyzer = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None)
 
 # Creating a FastAPI app
+
 class Entry(BaseModel):
     text: str
     
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.post("/analyze-emotion")
 def analyze_emotion(entry: Entry):
--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".scan-button img").addEventListener("click", function() {
         document.querySelector(".spinner").style.display = "inline-block";
+        let entryText = document.querySelector(".textarea").value; 
+
+        fetch("http://127.0.0.1:8000/analyze-emotion", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ text: entryText })
+        })
+
+
         setTimeout(function() {
             document.querySelector(".spinner").style.display = "none";
             window.location.href = "result.html";
         }, 3000);
-        let entryText = document.querySelector(".textarea").value; 
         localStorage.setItem("entryText", entryText);
 
     });
